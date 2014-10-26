999537c2f2d14738d427ed70200f9d1b53f8e2bd
alejandroajhr <alejandro.ajhr@gmail.com>
2014-10-26 02:47:42 +0100

    FCM v1.1

--- project1/fcm.m
+++ project1/fcm.m
@@ -1,27 +1,30 @@
-function [cluster_vector, centroids, niters] = fcm(c,data,max_iters,epsilon,m)
+function [cluster_vector, centroids, niters, err] = fcm(data,c,max_iters,m,epsilon)
     niters = max_iters;
+    blank = ones(1,size(data,2));
     centroids = zeros(c,size(data,2));
-    blank = ones(1,size(data,2))
     n_samples = size(data,1);
     cluster_vector = rand(n_samples,c);
-    prev_centroids = ones(size(centroids))*realmax;
-    while (abs(sum(prev_centroids - centroids)) < epsilon) && (niters > 0)
+    prev_centroids = ones(c,size(data,2))*realmax;
+    while (abs(sum(sum(prev_centroids - centroids))) > epsilon) && (niters > 0)
         prev_centroids = centroids;
-        %Compute new centroids, vectorized
-        centroids = ((cluster_vector.^m)'*data) ./ sum((cluster_vector.^m)')*blank;
+        %Compute new centroids, vectorized, guaranteed
+        fuzzy_vector = (cluster_vector.^m)';
+        centroids = (fuzzy_vector*data) ./ (sum(fuzzy_vector,2)*blank);
                      
         for n = 1:n_samples
             for i = 1:c
                 dist_x_centi = pdist([data(n,:);centroids(i,:)],'euclidean');
                 dist_x = 0;
                 for j = 1:c
-                    dist_x = dist_x + (dist_x_centi/pdist([data(n,:);centroids(j,:)],'euclidean'));
+                    dist_x_centj = pdist([data(n,:);centroids(j,:)],'euclidean');
+                    dist_x = dist_x + ((dist_x_centi/dist_x_centj)^(2/(m-1)));
                 end
-                cluster_vector(n,c) = 1 / (dist_x)^(2/m-1);
+                cluster_vector(n,i) = 1 / dist_x;
             end
         end
         
         niters = niters - 1;
     end
     niters = max_iters-niters;
+    err = abs(sum(sum(prev_centroids - centroids)));
 end
