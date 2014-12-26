b1f4d3413ab272c31d11aa650db3a0b314b93c7f
alejandroajhr <alejandro.ajhr@gmail.com>
2014-12-26 15:08:34 +0100

    combinatooor uuh

--- work2/acbrAlgorithm.m
+++ work2/acbrAlgorithm.m
@@ -5,9 +5,6 @@
 % Performs a CBR to the given standarized data performing KNN on k
 % neighbors and using the provided ReuseStrategy for reuse of instances.
 function [STDData, Categories, Goodness, newClass] = acbrAlgorithm(STDData,Categories, Goodness, InitGoodness, Instance, InstanceClass, K, ReuseStrategy)
-    [knn,d] = acbrRetrievalPhase(STDData,Instance,K);
-    [instance,newClass] = acbrReusePhase(STDData, Categories, [knn',d'], ReuseStrategy);
-    [Goodness] = acbrReviewPhase(STDData, Categories, knn',d', Goodness, newClass,0.3);
-    [STDData,Categories] = acbrRetentionPhase(STDData, knn',d', Categories,Instance, InstanceClass, newClass, Goodness, InitGoodness, 'DD',0);
+    [STDData, Categories, Goodness, newClass] = cbrCombinator(STDData,Categories, Goodness, InitGoodness,  Instance, InstanceClass, K, ReuseStrategy, false, true, true);
 end
 
--- work2/weightedACBRAlgorithm.m
+++ work2/weightedACBRAlgorithm.m
@@ -5,9 +5,6 @@
 % Performs a CBR to the given standarized data performing KNN on k
 % neighbors and using the provided ReuseStrategy for reuse of instances.
 function [STDData, Categories, Goodness, newClass] = weightedACBRAlgorithm(STDData,Categories, Goodness, InitGoodness,  Instance, InstanceClass, K, ReuseStrategy)
-    [knn,d,weights] = weightedACBRRetrievalPhase(STDData,Instance,K);
-    [instance,newClass] = acbrReusePhase(STDData, Categories, [knn',d',weights'], ReuseStrategy);
-    [Goodness] = acbrReviewPhase(STDData, Categories, knn',d', Goodness, newClass,0.3);
-    [STDData,Categories] = acbrRetentionPhase(STDData, knn',d', Categories,Instance, InstanceClass, newClass, Goodness, InitGoodness,  'DD',0);
+    [STDData, Categories, Goodness, newClass] = cbrCombinator(STDData,Categories, Goodness, InitGoodness,  Instance, InstanceClass, K, ReuseStrategy, true, true, true);
 end
 
