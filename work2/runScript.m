c1a9266c9a7f46c5e3190bda269e9914dfb92eb5
Javier Fernandez <javier.fernandez@synergy-gb.com>
2014-12-28 17:26:48 +0100

    adding save data to datasets

--- work2/runScript.m
+++ work2/runScript.m
@@ -28,16 +28,19 @@ save('saved_data/ZOO-WRELIEF-results.mat','totalStats');
 disp('Generating data and figures for SEGMENT Database - Non Weighted');
 totalStats = testAllACBRCombinations('SEGMENT',false)
 graphACBRStats(totalStats,'SEGMENT')
+save('saved_data/SEGMENT-NW-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Fisher Selection
 disp(' Generating data and figures for SEGMENT Database - Weighted / Fisher Selection');
 totalStats = testAllACBRCombinations('SEGMENT',true,'FISHER')
 graphACBRStats(totalStats,'WSEGMENT-FISHER')
+save('saved_data/SEGMENT-WFISHER-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Relief Selection
 disp('Generating data and figures for SEGMENT Database - Weighted / Relief Selection');
 totalStats = testAllACBRCombinations('SEGMENT',true,'RELIEF')
 graphACBRStats(totalStats,'WSEGMENT-RELIEF')
+save('saved_data/SEGMENT-WRELIEF-results.mat','totalStats');
 
 
 %% PENBASED DATASET
@@ -45,16 +48,19 @@ graphACBRStats(totalStats,'WSEGMENT-RELIEF')
 disp('Generating data and figures for PENBASED Database - Non Weighted');
 totalStats = testAllACBRCombinations('PENBASED',false)
 graphACBRStats(totalStats,'PENBASED')
+save('saved_data/PENBASED-NW-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Fisher Selection
 disp(' Generating data and figures for PENBASED Database - Weighted / Fisher Selection');
 totalStats = testAllACBRCombinations('PENBASED',true,'FISHER')
 graphACBRStats(totalStats,'WPENBASED-FISHER')
+save('saved_data/PENBASED-WFISHER-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Relief Selection
 disp('Generating data and figures for PENBASED Database - Weighted / Relief Selection');
 totalStats = testAllACBRCombinations('PENBASED',true,'RELIEF')
 graphACBRStats(totalStats,'WPENBASED-RELIEF')
+save('saved_data/PENBASED-WRELIEF-results.mat','totalStats');
 
 
 %% WAVEFORM DATASET
@@ -62,15 +68,18 @@ graphACBRStats(totalStats,'WPENBASED-RELIEF')
 disp('Generating data and figures for WAVEFORM Database - Non Weighted');
 totalStats = testAllACBRCombinations('WAVEFORM',false)
 graphACBRStats(totalStats,'WAVEFORM')
+save('saved_data/WAVEFORM-NW-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Fisher Selection
 disp(' Generating data and figures for WAVEFORM Database - Weighted / Fisher Selection');
 totalStats = testAllACBRCombinations('WAVEFORM',true,'FISHER')
 graphACBRStats(totalStats,'WWAVEFORM-FISHER')
+save('saved_data/WAVEFORM-WFISHER-results.mat','totalStats');
 
 %% Generating data and figures for ZOO Database - Weighted / Relief Selection
 disp('Generating data and figures for WAVEFORM Database - Weighted / Relief Selection');
 totalStats = testAllACBRCombinations('WAVEFORM',true,'RELIEF')
 graphACBRStats(totalStats,'WWAVEFORM-RELIEF')
+save('saved_data/WAVEFORM-WRELIEF-results.mat','totalStats');
 
 
